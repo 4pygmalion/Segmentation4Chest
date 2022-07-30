fb619605d6557061920828433a57f38c4d86079e
4pygmalion <4pygmalion@naver.com>
2022-07-30 11:08:24 +0900

    issue #2: model

--- data/JSRT/dicom2png.py
+++ data/JSRT/dicom2png.py
@@ -8,12 +8,18 @@ def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-s", "--src_dir", type=str, help="source directory")
     parser.add_argument("-d", "--dst_dir", type=str, help="destination directory")
+    parser.add_argument("-r", "--resize", type=str, help="resizing image")
 
     return parser.parse_args()
 
-def dicom2png(src:str, dst:str) -> None:
+def dicom2png(src:str, dst:str, resize:tuple) -> None:
     dicom_ojb = dicom.dcmread(src)
-    plt.imsave(dst, cv2.normalize(dicom_ojb.pixel_array, None, 0, 255, cv2.NORM_MINMAX))
+    img = cv2.normalize(dicom_ojb.pixel_array, None, 0, 255, cv2.NORM_MINMAX)
+
+    if resize:
+        img = cv2.resize(img, dsize=resize, interpolation=cv2.INTER_AREA)
+
+    plt.imsave(dst, img)
 
     return 
 
@@ -25,9 +31,11 @@ if __name__ == "__main__":
     ROOT_DIR = os.path.dirname(DATA_DIR)
     DST_DIR = ARGS.dst_dir
 
+    RESIZE = eval(ARGS.resize)
+
     os.makedirs(DST_DIR, exist_ok=True)
 
     for dcm in os.listdir(DATA_DIR):
         src = os.path.join(DATA_DIR, dcm)
         dst = os.path.join(DST_DIR, dcm.replace(".dcm", ".png"))
-        dicom2png(src, dst)
+        dicom2png(src, dst, RESIZE)
